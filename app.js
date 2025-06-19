33ac19d28719fd7bd60ddd3932969c0638464e41
Kanhu-Polai-22 <polaikanhu6@gmail.com>
2025-06-19 18:27:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,19 @@
 // add new features - button
 // add new features - form
 
+function addTask() {
+  const taskInput = document.getElementById('task');
+  const task = taskInput.value.trim();
+
+  if (task !== '') {
+    const li = document.createElement('li');
+    li.innerHTML = `${task} <button class="delete" onclick="deleteTask(this)">Delete</button>`;
+    document.getElementById('list').appendChild(li);
+    taskInput.value = '';
+  }
+}
+
+function deleteTask(button) {
+  const li = button.parentElement;
+  li.remove();
+}
